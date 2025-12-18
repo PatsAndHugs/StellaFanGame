40f5a8840c090c0c4ddcb541c650c2ee882b3897
P&H <jocast.apps@gmail.com>
2025-12-18 15:31:05 +0800

    added interface for minigames

---
+++ Source/StellaFanGame/Private/Interactions/Interfaces/MinigameInterface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interactions/Interfaces/MinigameInterface.h"
+
+// Add default functionality here for any IMinigameInterface functions that are not pure virtual.
